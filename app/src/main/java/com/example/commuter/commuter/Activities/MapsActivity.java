4f1e790b7f0c384972931cca69e97da39ab27f17
Timothy Ho <tho.nyc26@gmail.com>
2017-05-27 21:34:38 -0400

    GoogleMapsAPI     - Removed ActionBar from MapsActivity class

--- app/src/main/java/com/example/commuter/commuter/Activities/MapsActivity.java
+++ app/src/main/java/com/example/commuter/commuter/Activities/MapsActivity.java
@@ -47,6 +47,7 @@ public class MapsActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
